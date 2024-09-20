076856ebdda01ac8163733cd5cb32f6df9e869d3
Midhun <midhunskani@gmail.com>
2024-09-20 10:20:12 +0530

    7

--- 7_Check if List is Subset of another List.py
+++ 7_Check if List is Subset of another List.py
@@ -1,3 +1,16 @@
 def is_subset(lst1, lst2):
-    # Your code goes here
-    pass
+    if len(lst1) == 0:
+        return True
+    if len(lst1) > len(lst2):
+        return False
+    length = len(lst1)
+    for i in range(len(lst2) - length + 1):
+        sub_li = lst2[i:length + i]
+        if sub_li == lst1:
+            return True
+    return False
+
+
+lst1 = [1, 2, 3]
+lst2 = [1, 2, 3, 4, 5]
+print(is_subset(lst1, lst2))
