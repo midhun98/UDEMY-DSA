ea08c54d24cbe9000170b026dd2fa91208513098
Midhun <midhunskani@gmail.com>
2024-09-26 13:35:26 +0530

    18 selection_sort

---
+++ 18_selection_sort.py
@@ -0,0 +1,12 @@
+def selection_sort(arr):
+    for i in range(len(arr) - 1):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+lst = [64, 34, 25, 11, 22, 11, 90]
+print(selection_sort(lst))
